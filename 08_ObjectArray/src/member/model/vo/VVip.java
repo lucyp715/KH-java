4851499800efb5c27224876aa564a99db98cbb41
lucyp715 <s99026@naver.com>
2020-12-02 23:06:22 +0900

    pointarray & override

---
+++ 08_ObjectArray/src/member/model/vo/VVip.java
@@ -0,0 +1,18 @@
+package member.model.vo;
+
+public class VVip extends Member{
+
+	public VVip() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public VVip(String name, String grade, int point) {
+		super(name, grade, point);
+	
+	}
+	@Override
+	public double getEjapoint() {
+		return getPoint() * 0.15;
+	}
+}
---
+++ 08_ObjectArray/src/member/model/vo/Vip.java
@@ -0,0 +1,18 @@
+package member.model.vo;
+
+public class Vip extends Member{
+
+	public Vip() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Vip(String name, String grade, int point) {
+		super(name, grade, point);
+	
+	}
+	@Override
+	public double getEjapoint() {
+		return getPoint() * 0.1;
+
+	}
+}
